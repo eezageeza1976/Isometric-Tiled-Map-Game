dc466dd34b30db61e23bf4e89eceb51ae03f4016
eezageeza1976 <89101225+eezageeza1976@users.noreply.github.com>
2022-10-17 13:52:59 +0100

    Update settings.py

--- settings.py
+++ settings.py
@@ -31,13 +31,57 @@ WALL_IMG = 'tile_06.png'
 # LEVEL_MAPS = ['House.tmx', 'example.tmx']
 LEVEL_MAPS = 'dungeon.tmx'
 # player settings
-PLAYER_SPEED = 300
+PLAYER_SPEED = 100
+
+PLAYER_WALKING = 1.6
+
 PLAYER_ROT_SPEED = 250
 PLAYER_IMG = 'crusader_idle_00000.png'
-PLAYER_HIT_RECT = pg.Rect(0, 0, 35, 35)
+PLAYER_HIT_RECT = pg.Rect(0, 0, 60, 120)
 BARREL_OFFSET = vec(30, 10)
 PLAYER_HEALTH = 100
-PLAYER_SCALE = (200, 150)
+PLAYER_SCALE = (180, 120)
+PLAYER_WALK = {}
+PLAYER_WALK['southeast'] = ['walk/crusader_walk_10000.png', 'walk/crusader_walk_10001.png', 'walk/crusader_walk_10002.png',
+                             'walk/crusader_walk_10003.png', 'walk/crusader_walk_10004.png', 'walk/crusader_walk_10005.png',
+                             'walk/crusader_walk_10006.png', 'walk/crusader_walk_10007.png', 'walk/crusader_walk_10008.png',
+                             'walk/crusader_walk_10009.png', 'walk/crusader_walk_10010.png', 'walk/crusader_walk_10011.png',
+                             'walk/crusader_walk_10012.png', 'walk/crusader_walk_10013.png', 'walk/crusader_walk_10014.png']
+PLAYER_WALK['northeast'] = ['walk/crusader_walk_30000.png', 'walk/crusader_walk_30001.png', 'walk/crusader_walk_30002.png',
+                             'walk/crusader_walk_30003.png', 'walk/crusader_walk_30004.png', 'walk/crusader_walk_30005.png',
+                             'walk/crusader_walk_30006.png', 'walk/crusader_walk_30007.png', 'walk/crusader_walk_30008.png',
+                             'walk/crusader_walk_30009.png', 'walk/crusader_walk_30010.png', 'walk/crusader_walk_30011.png',
+                             'walk/crusader_walk_30012.png', 'walk/crusader_walk_30013.png', 'walk/crusader_walk_30014.png']
+PLAYER_WALK['southwest'] = ['walk/crusader_walk_70000.png', 'walk/crusader_walk_70001.png', 'walk/crusader_walk_70002.png',
+                             'walk/crusader_walk_70003.png', 'walk/crusader_walk_70004.png', 'walk/crusader_walk_70005.png',
+                             'walk/crusader_walk_70006.png', 'walk/crusader_walk_70007.png', 'walk/crusader_walk_70008.png',
+                             'walk/crusader_walk_70009.png', 'walk/crusader_walk_70010.png', 'walk/crusader_walk_70011.png',
+                             'walk/crusader_walk_70012.png', 'walk/crusader_walk_70013.png', 'walk/crusader_walk_70014.png']
+PLAYER_WALK['northwest'] = ['walk/crusader_walk_50000.png', 'walk/crusader_walk_50001.png', 'walk/crusader_walk_50002.png',
+                             'walk/crusader_walk_50003.png', 'walk/crusader_walk_50004.png', 'walk/crusader_walk_50005.png',
+                             'walk/crusader_walk_50006.png', 'walk/crusader_walk_50007.png', 'walk/crusader_walk_50008.png',
+                             'walk/crusader_walk_50009.png', 'walk/crusader_walk_50010.png', 'walk/crusader_walk_50011.png',
+                             'walk/crusader_walk_50012.png', 'walk/crusader_walk_50013.png', 'walk/crusader_walk_50014.png']
+PLAYER_WALK['east'] = ['walk/crusader_walk_20000.png', 'walk/crusader_walk_20001.png', 'walk/crusader_walk_20002.png',
+                             'walk/crusader_walk_20003.png', 'walk/crusader_walk_20004.png', 'walk/crusader_walk_20005.png',
+                             'walk/crusader_walk_20006.png', 'walk/crusader_walk_20007.png', 'walk/crusader_walk_20008.png',
+                             'walk/crusader_walk_20009.png', 'walk/crusader_walk_20010.png', 'walk/crusader_walk_20011.png',
+                             'walk/crusader_walk_20012.png', 'walk/crusader_walk_20013.png', 'walk/crusader_walk_20014.png']
+PLAYER_WALK['north'] = ['walk/crusader_walk_40000.png', 'walk/crusader_walk_40001.png', 'walk/crusader_walk_40002.png',
+                             'walk/crusader_walk_40003.png', 'walk/crusader_walk_40004.png', 'walk/crusader_walk_40005.png',
+                             'walk/crusader_walk_40006.png', 'walk/crusader_walk_40007.png', 'walk/crusader_walk_40008.png',
+                             'walk/crusader_walk_40009.png', 'walk/crusader_walk_40010.png', 'walk/crusader_walk_40011.png',
+                             'walk/crusader_walk_40012.png', 'walk/crusader_walk_40013.png', 'walk/crusader_walk_40014.png']
+PLAYER_WALK['west'] = ['walk/crusader_walk_60000.png', 'walk/crusader_walk_60001.png', 'walk/crusader_walk_60002.png',
+                             'walk/crusader_walk_60003.png', 'walk/crusader_walk_60004.png', 'walk/crusader_walk_60005.png',
+                             'walk/crusader_walk_60006.png', 'walk/crusader_walk_60007.png', 'walk/crusader_walk_60008.png',
+                             'walk/crusader_walk_60009.png', 'walk/crusader_walk_60010.png', 'walk/crusader_walk_60011.png',
+                             'walk/crusader_walk_60012.png', 'walk/crusader_walk_60013.png', 'walk/crusader_walk_60014.png']
+PLAYER_WALK['south'] = ['walk/crusader_walk_00000.png', 'walk/crusader_walk_00001.png', 'walk/crusader_walk_00002.png',
+                             'walk/crusader_walk_00003.png', 'walk/crusader_walk_00004.png', 'walk/crusader_walk_00005.png',
+                             'walk/crusader_walk_00006.png', 'walk/crusader_walk_00007.png', 'walk/crusader_walk_00008.png',
+                             'walk/crusader_walk_00009.png', 'walk/crusader_walk_00010.png', 'walk/crusader_walk_00011.png',
+                             'walk/crusader_walk_00012.png', 'walk/crusader_walk_00013.png', 'walk/crusader_walk_00014.png']
 
 # Weapon settings
 BULLET_IMG = 'bullet.png'
@@ -80,7 +124,8 @@ DAMAGE_ALPHA = [i for i in range(0, 255, 55)]
 # Layers
 WALL_LAYER = 3
 PLAYER_LAYER = 2
-GROUND_LAYER = 1
+BELOW_PLAYER = 1
+GROUND_LAYER = 0
 BULLET_LAYER = 3
 MOB_LAYER = 2
 EFFECTS_LAYER = 4
@@ -106,4 +151,5 @@ WEAPON_SOUNDS = {'pistol': ['weapons/pistol.wav'],
 EFFECTS_SOUNDS = {'level_start': 'level_start.wav',
                   'health_up': 'health_pack.wav',
                   'gun_pickup': 'gun_pickup.wav'}
+
 
