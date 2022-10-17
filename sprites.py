082dd4b694770d621fd0fb01182697263e039cb6
eezageeza1976 <89101225+eezageeza1976@users.noreply.github.com>
2022-10-17 13:54:14 +0100

    Update sprites.py

--- sprites.py
+++ sprites.py
@@ -1,85 +1,182 @@
 import pygame as pg
 import pytmx
+from tiledmap import *
 from settings import *
+from camera import *
 vec = pg.math.Vector2
 
+def collide_hit_rect(player, wall):
+    return player.hit_rect.colliderect(wall.rect)
+
+def collide_with_walls(sprite, group, dir):
+    if dir == 'x':
+        hits = pg.sprite.spritecollide(sprite, group, False, collide_hit_rect)
+        if hits:
+            if hits[0].rect.centerx > sprite.rect.centerx:
+                sprite.pos.x = hits[0].rect.left - sprite.hit_rect.width / 2
+            if hits[0].rect.centerx < sprite.rect.centerx:
+                sprite.pos.x = hits[0].rect.right + sprite.hit_rect.width / 2
+            sprite.vel.x = 0
+            sprite.hit_rect.centerx = sprite.pos.x
+    if dir == 'y':
+        hits = pg.sprite.spritecollide(sprite, group, False, collide_hit_rect)
+        if hits:
+            if hits[0].rect.centery > sprite.rect.centery:
+                sprite.pos.y = hits[0].rect.top - sprite.hit_rect.height / 2
+            if hits[0].rect.centery < sprite.rect.centery:
+                sprite.pos.y = hits[0].rect.bottom + sprite.hit_rect.height / 2
+            sprite.vel.y = 0
+            sprite.hit_rect.centery = sprite.pos.y
+            
 class Player(pg.sprite.Sprite):
     def __init__(self, game, x, y):
-        self._layer = PLAYER_LAYER
-        self.groups = game.all_sprites
-        pg.sprite.Sprite.__init__(self, self.groups)
+        self.groups = game.all_sprites, game.player_sprite, game.wall_sprites     
+#         self.image = game.player_img
         self.game = game
-        self.image = self.game.player_img
+        self.load_images()
+        self.image = self.walk_e_imgs[0]
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
+        self._layer = self.rect.bottom
+        self.hit_rect = PLAYER_HIT_RECT
+        self.hit_rect.center = self.rect.center
+        pg.sprite.Sprite.__init__(self, self.groups)
         self.vel = vec(0, 0)
         self.pos = vec(x, y)
         self.rot = 0
-        
+        self.working_frames = self.walk_e_imgs
+        self.player_speed = PLAYER_WALKING
+        self.last_update = 0
+        self.current_frame = 0
+        self.walking = False
+    
+    def load_images(self):
+        self.walk_se_imgs = self.game.image_loader.get_image(PLAYER_WALK, 'southeast', True)
+        self.walk_ne_imgs = self.game.image_loader.get_image(PLAYER_WALK, 'northeast', True)
+        self.walk_sw_imgs = self.game.image_loader.get_image(PLAYER_WALK, 'southwest', True)
+        self.walk_nw_imgs = self.game.image_loader.get_image(PLAYER_WALK, 'northwest', True)
+        self.walk_e_imgs = self.game.image_loader.get_image(PLAYER_WALK, 'east', True)
+        self.walk_n_imgs = self.game.image_loader.get_image(PLAYER_WALK, 'north', True)
+        self.walk_w_imgs = self.game.image_loader.get_image(PLAYER_WALK, 'west', True)
+        self.walk_s_imgs = self.game.image_loader.get_image(PLAYER_WALK, 'south', True)
+    
     def update(self):
         self.get_keys()
         self.rect = self.image.get_rect()
         self.rect.center = self.pos
         self.pos += self.vel * self.game.dt
+        self.hit_rect.centerx = self.pos.x
+#         collide_with_walls(self, self.game.wall_sprites, 'x')
+        self.hit_rect.centery = self.pos.y
+#         collide_with_walls(self, self.game.wall_sprites, 'y')
+        self.rect.center = self.hit_rect.center
         
     
     def get_keys(self):
-        self.rot_speed = 0
+        self.animate()
         self.vel = vec(0, 0)
         keys = pg.key.get_pressed()
         if keys[pg.K_LEFT] or keys[pg.K_a]:
+            self.walking = True
+            self.working_frames = self.walk_nw_imgs
             self.vel = self.xy_to_iso(vec(-PLAYER_SPEED, 0))
         if keys[pg.K_RIGHT] or keys[pg.K_d]:
+            self.walking = True
+            self.working_frames = self.walk_se_imgs
             self.vel = self.xy_to_iso(vec(PLAYER_SPEED, 0))
         if keys[pg.K_UP] or keys[pg.K_w]:
+            self.walking = True
+            self.working_frames = self.walk_ne_imgs
             self.vel = self.xy_to_iso(vec(0, -PLAYER_SPEED))
         if keys[pg.K_DOWN] or keys[pg.K_s]:
+            self.walking = True
+            self.working_frames = self.walk_sw_imgs
             self.vel = self.xy_to_iso(vec(0, PLAYER_SPEED))
             
     def xy_to_iso(self, v):
         return vec((v.x - v.y), (v.x + v.y) / 2)      
-
-def isometric_render(layer, tiled_map):
-    temp_surface = pg.Surface((tiled_map.width, tiled_map.height))
-    current_layer = tiled_map.tmxdata.get_layer_by_name(layer)
-    ti = tiled_map.tmxdata.get_tile_image_by_gid           
-    if isinstance(current_layer, pytmx.TiledTileLayer):
-        for x, y, gid in current_layer:
-            if gid != 0:
-                starting_x = ((tiled_map.width / 2) - tiled_map.tilewidth / 2)
-                offset = vec(((x - y) * tiled_map.tilewidth / 2) + starting_x,
-                             ((x + y) * tiled_map.tileheight / 2) - 370)
-                tile = ti(gid)
-                
-                if tile:
-                    temp_surface.blit(tile, (offset.x, offset.y))
-    return temp_surface
-                            
-class Ground(pg.sprite.Sprite):
-    def __init__(self, game, tiled_map):
-        self._layer = GROUND_LAYER
-        self.groups = game.all_sprites
-        pg.sprite.Sprite.__init__(self, self.groups)
-        self.game = game
-        self.image = isometric_render('Ground', tiled_map)
-        self.rect = self.image.get_rect()
+                           
+    def animate(self):
+        now = pg.time.get_ticks()
             
-class Walls(pg.sprite.Sprite):
-    def __init__(self, game, tiled_map):
-        self._layer = WALL_LAYER
-        self.groups = game.all_sprites, game.wall_sprites
-        pg.sprite.Sprite.__init__(self, self.groups)
-        self.game = game
-        self.image = isometric_render('Walls', tiled_map)
+        if now - self.last_update > 30 * self.game.dt:
+            if not self.walking:
+                self.current_frame = 0
+                self.image = self.working_frames[self.current_frame]
+            elif self.walking:
+                self.last_update = now
+                self.current_frame = (self.current_frame + 1) % len(self.working_frames)
+                self.image = self.working_frames[self.current_frame]
+                self.walking = False
+            
+class Wall(pg.sprite.Sprite):
+    def __init__(self, game, x, y, image):
+        self.groups = game.all_sprites, game.wall_sprites        
+        self.image = image
         self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
-        
-class obsticle(pg.sprite.Sprite):
-    def __init__(self, game, tiled_map):
-        self._layer = WALL_LAYER
-        self.groups = game.all_sprites
+        self.pos = vec(x, y)
+        self.rect.topleft = self.pos
+        self._layer = self.rect.bottom
         pg.sprite.Sprite.__init__(self, self.groups)
         self.game = game
-        self.image = isometric_render('Obsticles', tiled_map)
+        self.mask = pg.mask.from_surface(self.image)
+        
+    def update(self):
         self.rect = self.image.get_rect()
-        self.image.fill(GREEN)
+        self.rect.topleft = self.pos
+
+class Tree(pg.sprite.Sprite):
+    def __init__(self, game, x, y, image):
+        self.groups = game.all_sprites, game.tree_sprites        
+        self.image = image
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
+        self.rect.bottom = self.rect.bottom - 30
+        self.pos = vec(x, y)
+        self.rect.topleft = self.pos
+        self._layer = self.rect.bottom
+        pg.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.mask = pg.mask.from_surface(self.image)
+        
+    def update(self):
+        self.rect = self.image.get_rect()
+        print(self.rect)
+        self.rect.bottom = self.rect.bottom - 30
+        print(self.rect)
+        self.rect.topleft = self.pos
+        
+# class Obsticle(pg.sprite.Sprite):
+#     def __init__(self, game, x, y, w, h):
+#         self._layer = GROUND_LAYER
+#         self.groups = game.objects_sprites
+#         pg.sprite.Sprite.__init__(self, self.groups)
+#         self.game = game
+#         self.rect = pg.Rect(x, y, w, h)
+# #         self.rect = pg.transform.rotate(self.rect, 45)
+#         self.x = x
+#         self.y = y
+#         self.rect.x = x
+#         self.rect.y = y
+        
+class Obsticle(pg.sprite.Sprite):
+    def __init__(self, game, point_list):
+        self.groups = game.obs_sprites
+        pg.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.points = point_list
+        self.pos1 = vec(point_list[0][0], point_list[0][1])
+        self.pos2 = vec(point_list[1][0], point_list[1][1])
+        self.pos3 = vec(point_list[2][0], point_list[2][1])
+        self.pos4 = vec(point_list[3][0], point_list[3][1])
+        
+    def update(self):
+        self.points[0].topleft = self.pos1
+        self.points[1].topleft = self.pos2
+        self.points[2].topleft = self.pos3
+        self.points[3].topleft = self.pos4
+
+        
+        
+
